6122c32247308b031412e6402b1f2a4af7abad78
Zeltrex10 <23viners@gmail.com>
2025-02-09 22:54:25 +0800

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBQA4nzC1QorjegYC1-kHLXHoBF9PHb4k",
-  authDomain: "facialrecognitionidscanner.firebaseapp.com",
-  projectId: "facialrecognitionidscanner",
-  storageBucket: "facialrecognitionidscanner.appspot.com",
-  messagingSenderId: "1028897436260",
-  appId: "1:1028897436260:web:569b6ea5454b06437c2571",
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSENGER_ID",
+  appId: "APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
